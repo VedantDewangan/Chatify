d39da992f15f1057759eb4fb9675e2934015aed4
VedantDewangan <135214899+VedantDewangan@users.noreply.github.com>
2024-09-29 20:16:28 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -11,7 +11,7 @@ const jwtToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 30 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "none",
   });
 };
 
